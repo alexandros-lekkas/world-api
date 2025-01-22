89341e5cc6e671a7286e6450af3b5a428887bf79
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-01-22 17:14:53 -0600

    Rename allowed_origins to allowed_hosts in config and implement middleware to restrict Host header

--- api/main.py
+++ api/main.py
@@ -1,32 +1,48 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request, HTTPException
 import yaml
 
 from api.routes.countries import router as countries_router
 from api.routes.states import router as states_router
 from api.routes.cities import router as cities_router
 
-with open("config.yml", "r") as config_file:
+with open("api/config.yml", "r") as config_file:
     config = yaml.safe_load(config_file)
     
-allowed_origins = config["allowed_origins"]
+allowed_hosts= config["allowed_hosts"]
 
-if not allowed_origins or len(allowed_origins) == 0:
-    allowed_origins = ["*"]
+if (not allowed_hosts) or len(allowed_hosts) == 0:
+    allowed_hosts = ["*"]
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=allowed_origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(countries_router, prefix="/api", tags=["Countries"])
 app.include_router(states_router, prefix="/api", tags=["States"])
 app.include_router(cities_router, prefix="/api", tags=["Cities"])
 
+@app.middleware("http")
+async def restrict_host_header(request: Request, call_next):
+    """
+    Middleware that restricts the Host header to the allowed origins.
+    
+    Args:
+        request (Request): The request object.
+        call_next (function): The next middleware function.
+    
+    Returns (Response): The response object.
+    """
+    host = request.headers.get("host")
+    
+    if host not in allowed_hosts and "*" not in allowed_hosts:
+        raise HTTPException(status_code=400, detail="Host header invalid.")
+    
+    response = await call_next(request)
+    return response
+
 @app.get("/")
-def read_root():
+def welcome():
+    """
+    Root endpoint that returns a welcome message.
+    
+    Returns (dict): A welcome message.
+    """
     return {"message": "Welcome to the World API!"}
